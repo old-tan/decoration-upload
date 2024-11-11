095aa63d8290d508ef768b326d37b9dcd0189b8c
Airy Tan <674967815@qq.com>
2024-11-11 16:00:50 +0800

    chore: update .gitignore

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const hooks = require('feathers-hooks');
 const bodyParser = require('body-parser');
 const handler = require('feathers-errors/handler');
 const multer = require('multer');
-const multipartMiddleware = multer({
-    dest: 'temp/'  // 临时存储文件目录
-});
+const multipartMiddleware = multer();
 const dauria = require('dauria');
 
 // feathers-blob service
@@ -17,17 +15,19 @@ const blobService = require('feathers-blob');
 // Here we initialize a FileSystem storage,
 // but you can use feathers-blob with any other
 // storage service like AWS or Google Drive.
-const fsbs = require('fs-blob-store');
-const fs = require('fs');
-const path = require('path');
-const blobStorage = fsbs(__dirname + '/uploads');
+const fs = require('fs-blob-store');
+const blobStorage = fs(__dirname + '/uploads');
+const cors = require('cors');
+
+
 
 
 // Feathers app
 const app = feathers();
 
 // Serve our index page
-app.use('/', serveStatic(__dirname))
+console.log('__dirname---',__dirname)
+app.use('/', serveStatic(__dirname + '/public/index.html'))
 // Parse HTTP JSON bodies
 app.use(bodyParser.json({limit: '10mb'}));
 // Parse URL-encoded params
@@ -38,6 +38,9 @@ app.configure(hooks());
 app.configure(rest());
 // Configure Socket.io real-time APIs
 app.configure(socketio());
+
+// 配置 CORS 中间件
+app.use(cors())
 
 
 // Upload Service with multipart support
@@ -50,45 +53,9 @@ app.use('/uploads',
 
     // another middleware, this time to
     // transfer the received file to feathers
-    // function(req,res,next){
-    //     req.feathers.file = req.file;
-    //     next();
-    // },
-    function (req,res,next)  {
-        console.log('req------------------',req.file)
-        // // req.feathers.file = req.file;
-        // const targetPath = path.join(__dirname, 'uploads', req.file.originalname);
-        // console.log('targetPath---',targetPath)
-        // // // 确保目标目录存在
-        //  fs.promises.mkdir(path.dirname(targetPath), { recursive: true });
-    
-        // // // 使用流方式将临时文件写入目标文件
-        // const readStream = fs.createReadStream(req.file.path);
-        // const writeStream = fs.createWriteStream(targetPath);
-    
-        // // // 处理写入完成后的清理工作
-        // writeStream.on('finish', async () => {
-        //     console.log('finish------------')
-        //     // 删除临时文件
-        //     await fs.promises.unlink(req.file.path);
-        //     // 返回响应
-        //     res.json({ message: 'File uploaded successfully', fileName: req.file.originalname });
-        //     // next()
-        // });
-    
-        // // 处理错误
-        // readStream.on('error', (err) => {
-        //     console.log('read---error------',err)
-        //     next(err)
-        // });
-        // writeStream.on('error', (err) => {
-        //     console.log('write---error------',err)
-        //     next(err)
-        // });
-    
-        // // // 开始传输文件
-        // readStream.pipe(writeStream);
-        // // req.feathers.file = req.file
+    function(req,res,next){
+        req.feathers.file = req.file;
+        next();
     },
     blobService({Model: blobStorage})
 );
@@ -97,23 +64,22 @@ app.use('/uploads',
 // and turn it into a datauri,
 // transparently getting feathers-blob
 // to work with multipart file uploads
-// app.service('/uploads').before({
-//     create: [
-//         function(hook) {
-//             console.log('==========================',hook.params.file)
-//             if (hook.params.file){
-//                 const file = hook.params.file;
-//                 const uri = dauria.getBase64DataURI(file.buffer, file.mimetype);
-//                 hook.data = {uri: uri};
-//             }
-//         }
-//     ]
-// });`
+app.service('/uploads').before({
+    create: [
+        function(hook) {
+            if (!hook.data.uri && hook.params.file){
+                const file = hook.params.file;
+                const uri = dauria.getBase64DataURI(file.buffer, file.mimetype);
+                hook.data = {uri: uri};
+            }
+        }
+    ]
+});
 
 // Register a nicer error handler than the default Express one
 app.use(handler());
 
 // Start the server
-app.listen(3030, function(){
-    console.log('Feathers app started at localhost:3030')
+app.listen(3031, function(){
+    console.log('Feathers app started at localhost:3031')
 });
